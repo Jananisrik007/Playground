ad1bf1fc822fcc88ee196c676ce6a4fce5ebaba4
Jananisrik007 <52148760+Jananisrik007@users.noreply.github.com>
2019-07-25 22:06:27 +0530

    Test code pushed to repository.

---
+++ Arc length of a circle/Main.java
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float r,ca;
+float l;
+scanf("%f %f",&r,&ca);
+l=2*3.14*r*(ca/360);
+printf("%0.2f",l);
+return 0;
+}
---
+++ Multiply two numbers/Main.java
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+  int a=63,f=81,mul;
+  mul=a*f;
+  printf("%d",mul);
+  return 0;
+}
